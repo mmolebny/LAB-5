cff57972e839256167fa99a2ce9d2e217a87a738
max <mmolebny@gmail.com>
2026-04-23 01:15:28 +0300

    asynchronous version with threading

--- LAB 5.py
+++ LAB 5.py
@@ -1,6 +1,6 @@
 import threading
 import time
-
+import asyncio
 
 class AsyncMapper:
     def __init__(self, data):
@@ -19,6 +19,23 @@ class AsyncMapper:
             for item in self.data:
                 my_results.append(simple_func(item))
             on_finish(my_results)
-
         t = threading.Thread(target=worker)
         t.start()
+
+    def map_promise(self, async_func):
+        loop = asyncio.get_event_loop()
+        f = loop.create_future()
+        async def run_and_set():
+            items = []
+            for x in self.data:
+                items.append(await async_func(x))
+            f.set_result(items)
+        asyncio.ensure_future(run_and_set())
+        return f
+
+    async def map_async(self, async_func):
+        out = []
+        for i in self.data:
+            val = await async_func(i)
+            out.append(val)
+        return out
